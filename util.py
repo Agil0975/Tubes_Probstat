a8cac3ebf144c155722ea874a8eb8d82738ff7c6
Agil Fadillah Sabri <agilfadillahsabri03@gmail.com>
2024-05-22 23:03:42 +0700

    feat: no 2, no 3

--- util.py
+++ util.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from collections import defaultdict
 
-data = pd.read_csv("phone.csv")
+DATA = pd.read_csv("phone.csv")
 
 # variabel global
-n = len(data)                           # jumlah data
+n = len(DATA)                           # jumlah data
 template = "{:<20} | {:<30} | {:<30}"   # template untuk print
 
 # Rumus untuk menghitung data numerik
 # Mean
-def myMean(atribut):
+def myMean(data, atribut):
     # mean = Σx / n
     jumlah = 0
     for i in range(n):
@@ -17,7 +17,7 @@ def myMean(atribut):
     return jumlah/n
 
 # Median
-def myMedian(atribut):
+def myMedian(data, atribut):
     sorted_data = sorted(data[atribut])
     mid = n // 2
     
@@ -29,7 +29,7 @@ def myMedian(atribut):
     return median
 
 # Modus
-def myModus(atribut, tolerance=1e-5):
+def myModus(data, atribut, tolerance=1e-5):
     frequency = defaultdict(int)
     
     for num in data[atribut]:
@@ -48,36 +48,36 @@ def myModus(atribut, tolerance=1e-5):
     return modes[0]
 
 # Maximum, Minimum, Range
-def myMax(atribut):
+def myMax(data, atribut):
     max = data[atribut][0]
     for i in range(1, n):
         if data[atribut][i] > max:
             max = data[atribut][i]
     return max
 
-def myMin(atribut):
+def myMin(data, atribut):
     min = data[atribut][0]
     for i in range(1, n):
         if data[atribut][i] < min:
             min = data[atribut][i]
     return min
 
-def myRange(atribut):
+def myRange(data, atribut):
     # range = max - min
-    return myMax(atribut) - myMin(atribut)
+    return myMax(data, atribut) - myMin(data, atribut)
 
 # Variance dan Standard Deviation
-def myVariance(atribut):
+def myVariance(data, atribut):
     # variance = Σ(x - mean)^2 / (n - 1)
-    mean = myMean(atribut)
+    mean = myMean(data, atribut)
     jumlah = 0
     for i in range(n):
         jumlah += (data[atribut][i] - mean) ** 2
     return jumlah / (n - 1)
 
-def myStdDev(atribut):
+def myStdDev(data, atribut):
     # stdDev = sqrt(variance)
-    return myVariance(atribut) ** 0.5
+    return myVariance(data, atribut) ** 0.5
 
 # Quartile dan Interquartile Range
 def percentile(data, p):
@@ -89,25 +89,25 @@ def percentile(data, p):
     else:
         return data[f]
 
-def myQ1(atribut):
+def myQ1(data, atribut):
     data_sorted = sorted(data[atribut])
     return percentile(data_sorted, 25)
 
-def myQ3(atribut):
+def myQ3(data, atribut):
     data_sorted = sorted(data[atribut])
     return percentile(data_sorted, 75)
     
-def myIQR(atribut):
-    return myQ3(atribut) - myQ1(atribut)
+def myIQR(data, atribut):
+    return myQ3(data, atribut) - myQ1(data, atribut)
 
 # Skewness
 # https://www.slideshare.net/slideshow/3-skewness-kurtosispptx/255323562
-def mySkewness(atribut):
+def mySkewness(data, atribut):
     # koefisien_skewness = Σ((x - mean) / stdDev)^3
     # pengali = n / ((n - 1) * (n - 2))
     # skewness = pengali * koefisien_skewness
-    mean = myMean(atribut)
-    stdDev = myStdDev(atribut)
+    mean = myMean(data, atribut)
+    stdDev = myStdDev(data, atribut)
     koefisien_skewness = 0
     pengali = n / ((n - 1) * (n - 2))
     for i in range(n):
@@ -115,15 +115,32 @@ def mySkewness(atribut):
     return koefisien_skewness * pengali
 
 # Kurtosis
-def myKurtosis(atribut):
+def myKurtosis(data, atribut):
     # koefisien_kurtosis = Σ((x - mean) / stdDev)^4
     # pengali = n * (n + 1) / ((n - 1) * (n - 2) * (n - 3))
     # pengurang = 3 * (n - 1)² / ((n - 2) * (n - 3))
-    mean = myMean(atribut)
-    stdDev = myStdDev(atribut)
+    mean = myMean(data, atribut)
+    stdDev = myStdDev(data, atribut)
     koefisien_kurtosis = 0
     pengali = n*(n + 1) / ((n - 1) * (n - 2) * (n - 3))
     pengurang = 3 * (n - 1) ** 2 / ((n - 2) * (n - 3))
     for i in range(n):
         koefisien_kurtosis += (((data[atribut][i] - mean) / stdDev) ** 4)
     return pengali * koefisien_kurtosis - pengurang
+
+# Print Deskripsi Data
+def printDescriptiveStatistics(data, atribut):
+    print("Deskriptif Statistik | Fungsi Sendiri                 | Fungsi Library")
+    print(template.format("Mean", myMean(data, atribut), data[atribut].mean()))
+    print(template.format("Median", myMedian(data, atribut), data[atribut].median()))
+    print(template.format("Modus", myModus(data, atribut), data[atribut].mode()[0]))
+    print(template.format("Standar Deviasi", myStdDev(data, atribut), data[atribut].std()))
+    print(template.format("Variansi", myVariance(data, atribut), data[atribut].var()))
+    print(template.format("Range", myRange(data, atribut), data[atribut].max() - data[atribut].min()))
+    print(template.format("Minimum", myMin(data, atribut), data[atribut].min()))
+    print(template.format("Maksimum", myMax(data, atribut), data[atribut].max()))
+    print(template.format("Q1", myQ1(data, atribut), data[atribut].quantile(0.25)))
+    print(template.format("Q3", myQ3(data, atribut), data[atribut].quantile(0.75)))
+    print(template.format("IQR", myIQR(data, atribut), data[atribut].quantile(0.75) - data[atribut].quantile(0.25)))
+    print(template.format("Skewness", mySkewness(data, atribut), data[atribut].skew()))
+    print(template.format("Kurtosis", myKurtosis(data, atribut), data[atribut].kurt()))
